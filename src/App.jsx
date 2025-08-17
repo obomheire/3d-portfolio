634794a448d67be655fbbd5ab5e8a775e4492694
Zack Bello <zackoverflow@Zacks-MacBook-Air.local>
2025-08-17 21:04:12 +0100

    Working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './sections/Hero'
 import AppShowcase from './sections/ShowcaseSection'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
     <main>
       <div>
+        <NavBar />
         <Hero />
         <AppShowcase />
       </div>
