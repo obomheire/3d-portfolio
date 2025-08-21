6b6da723c91d55bb1d619ba189d7e7007b304525
Zack Bello <zackoverflow@Zacks-MacBook-Air.local>
2025-08-21 21:30:08 +0100

    Added testionila section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Hero from './sections/Hero'
 import AppShowcase from './sections/AppShowcase'
 import NavBar from './components/NavBar'
 import LogoShowcase from './sections/LogoShowcase'
+import FeatureCards from './sections/FeatureCards'
+import Experience from './sections/Experience'
+import TechStack from './sections/TechStack'
+import Testimonials from './sections/Testimonials'
 
 const App = () => {
   return (
@@ -12,6 +16,10 @@ const App = () => {
         <Hero />
         <AppShowcase />
         <LogoShowcase />
+        <FeatureCards />
+        <Experience />
+        <TechStack />
+        <Testimonials />
       </div>
     </main>
   )
--- src/components/models/hero_models/HeroExperience.jsx
+++ src/components/models/hero_models/HeroExperience.jsx
@@ -27,7 +27,7 @@ const HeroExperience = () => {
 
       <Suspense fallback={null}>
         <HeroLights />
-        <Particles count={100} />
+        <Particles count={100} /> 
         <group
           scale={isMobile ? 0.7 : 1}
           position={[0, -3.5, 0]}
