bf6a0b389bf19e113ab4c06f65dd065bd335defb
Zack Bello <zackoverflow@Zacks-MacBook-Air.local>
2025-08-26 18:51:06 +0100

    Refactored

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -108,7 +108,8 @@ const Contact = () => {
             </div>
           </div>
           <div className="xl:col-span-7 min-h-96">
-            <div className="bg-[#cd7c2e] w-full h-full hover:cursor-grab rounded-3xl overflow-hidden">
+            {/* <div className="bg-[#cd7c2e] w-full h-full hover:cursor-grab rounded-3xl overflow-hidden"> */}
+            <div className="bg-[#ADD8E6] w-full h-full hover:cursor-grab rounded-3xl overflow-hidden">
               <ContactExperience />
             </div>
           </div>
